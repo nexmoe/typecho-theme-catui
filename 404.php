9e6dcee1424c92fc5268121ca7d7182e2a6b89be
折影轻梦 <776194970@qq.com>
2017-02-17 23:56:23 +0800

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php 
+/** 
+  * Cat UI 情托于物。人情冷暖，世态炎凉。 * 
+  * @package Cat UI 
+  * @author 折影轻梦 
+  * @version 0.9 
+  * @link http://i.chainwon.com/ 
+*/ 
+$this->need('header.php'); ?>
+<body>
+    <div id="content">
+        <?php $this->need('header-t.php'); ?>
+        <div class="container">
+            <article class="nofound"><p>内容被次元之门封印啦！</p></article>
+        </div>
+    </div>
+    <?php $this->need('footer.php'); ?>
+</body>
