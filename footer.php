5cb658d8665300bf46dcfa6fe6a3b1783f234993
折影轻梦 <nexmoe@qq.com>
2021-08-01 14:22:34 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@ $this->footer();
 ?>
 <div class="btn-top btn fa fa-angle-up btn-raised"></div>
 <div class="footer">
-    <div class="copyright container">&copy; <?php echo date("Y");?> <?php $this->options->title(); ?></div>
+    <div class="copyright container">&copy; <?php echo date("Y");?> <?php $this->options->title(); ?>Theme By <a href="https://github.com/theme-catui/typecho-theme-catui" target="_blank">Cat UI</a></div>
 </div>
 <script src="//cdn.bootcss.com/highlight.js/9.9.0/highlight.min.js"></script>
 <script src="//cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
